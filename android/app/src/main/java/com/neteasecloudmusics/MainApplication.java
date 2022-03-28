1f46e1bc93ad81ce951a61808293cdafee0f1035
xlz122 <122874722@qq.com>
2022-03-28 08:47:10 +0800

    chore: 修改应用名称

--- android/app/src/main/java/com/neteasecloudmusics/MainApplication.java
+++ android/app/src/main/java/com/neteasecloudmusics/MainApplication.java
@@ -1,4 +1,4 @@
-package com.neteasecloudmusics;
+package com.NeteaseCloudMusic;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.neteasecloudmusics.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.NeteaseCloudMusic.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- ios/NeteaseCloudMusicsTests/NeteaseCloudMusicsTests.m
+++ ios/NeteaseCloudMusicsTests/NeteaseCloudMusicsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface NeteaseCloudMusicsTests : XCTestCase
+@interface NeteaseCloudMusicTests : XCTestCase
 
 @end
 
-@implementation NeteaseCloudMusicsTests
+@implementation NeteaseCloudMusicTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
