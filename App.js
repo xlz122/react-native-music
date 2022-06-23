82b5624bf7eb921bcdb0beb4263351fe3dbdff2b
xlz122 <122874722@qq.com>
2022-06-23 16:43:48 +0800

    chore: react native版本更新至0.69.0

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import StackNavigator from './src/router/StackNavigator';
+// import { NavigationContainer } from '@react-navigation/native';
+// import StackNavigator from './src/router/StackNavigator';
 import { Provider } from 'react-redux';
 import store from './src/store/index';
 // 忽略react-native-gesture-handler警告
-import { LogBox } from 'react-native';
+import { LogBox, Text } from 'react-native';
 
 LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
@@ -14,9 +14,10 @@ LogBox.ignoreLogs([
 function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
+      <Text>hello world</Text>
+      {/* <NavigationContainer>
         <StackNavigator />
-      </NavigationContainer>
+      </NavigationContainer> */}
     </Provider>
   );
 }
